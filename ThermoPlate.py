b038c739d71bf0013752c53a4946c2a146453ea1
Hirofumi Shintaku <166378660+hiroshintaku@users.noreply.github.com>
2025-09-02 17:39:23 +0900

    regulate the temperature of ThermoPlate

---
+++ ThermoPlate.py
@@ -0,0 +1,42 @@
+from pymodbus.client import ModbusSerialClient
+
+class ThermoPlate(ModbusSerialClient):
+    def __init__(self, parent=None):
+        # Modbus RTUクライアント設定
+        self.client = ModbusSerialClient(
+            port="COM10",
+            baudrate=9600,
+            bytesize=8,
+            parity='E',    # Even parity
+            stopbits=1,
+            timeout=1
+        )
+        self.SLAVE_ID = 1       # 通信ユニットNo.（デフォルト1）
+        self.SV_ADDR  = 0x2103 #address of set point
+        self.CR_ADDR=0x2402 #address of current value
+           
+    def settemp(self,tmp):
+        self.SV_VALUE = tmp  # 設定温度
+        # 接続開始
+        if not self.client.connect():
+            print("Connection Failed")
+        
+        # レジスタに書き込み
+        result = self.client.write_register(address=self.SV_ADDR, value=self.SV_VALUE,device_id=1)
+        if result.isError():
+            print("Writing Failed", result)
+        else:
+            print("set temperature to {}℃".format(tmp/10))
+        self.client.close()
+    
+    def readtemp(self):
+        try:
+            result =self.client.read_holding_registers(address=self.CR_ADDR,count=1,device_id=1)
+            return float(result.registers[0])/10.0
+        except:
+            print("error")
+            return -1
+
+# TP=ThermoPlate()
+# print(TP.readtemp())
+# TP.settemp(400)
