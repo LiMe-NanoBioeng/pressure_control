23c45b70c0e7f3bb1a5282077e1d35a64463091d
hirofumi shintaku <hirofumishintaku@gmail.com>
2026-05-31 11:13:10 +0900

    bug fix in acquire_image
    typo aquire_image acquire_image

--- main.py
+++ main.py
@@ -245,7 +245,7 @@ class MainWindow(QtWidgets.QMainWindow):
                         NI.ArduinoAO(ui.vNumA, False, 0)
                         # operating =0
                 elif mode=="a": #acquire image
-                    from pycromanager import MagellanAcquisition,Core
+                    from pycromanager import Core
                     try:
                         core = Core()
                     #     # no need to use the normal "with" syntax because these acquisition are cleaned up automatically
@@ -255,7 +255,7 @@ class MainWindow(QtWidgets.QMainWindow):
                         pos_file = self.Pos_file_path
                         print(mda_file,pos_file)
                         acq = acq_pycromanager(mda_file,pos_file)
-                        acq = acq.aquire_image()
+                        acq = acq.acquire_image()
                         print('sucess_acquirment')
                     except:
                         print('false')
